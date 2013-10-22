92a85c0406e153652892523c70afdd61ed866a12
Ralf Eggert <ralf@travello.com>
2013-10-22 21:49:20 +0200

    Added empty Customer module

---
+++ module/Customer/Module.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Unit-Testing einer Zend Framework 2 Anwendung
+ *
+ * Zend Framework Session auf der International PHP Conference 2013 in München
+ *
+ * @package    Customer
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Ralf Eggert <r.eggert@travello.de>
+ * @link       http://www.ralfeggert.de/
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace Customer;
+
+/**
+ * Customer module class
+ *
+ * @package    Customer
+ */
+class Module
+{
+}
