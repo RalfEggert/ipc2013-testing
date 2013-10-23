dec3213cd90e9d00ce7aa717ee7c839a67afe482
Ralf Eggert <ralf@travello.com>
2013-10-23 19:22:10 +0200

    CustomerHydratorTest erstellt

---
+++ module/Customer/tests/CustomerTest/Hydrator/CustomerHydratorTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Unit-Testing einer Zend Framework 2 Anwendung
+ *
+ * Zend Framework Session auf der International PHP Conference 2013 in München
+ *
+ * @package    CustomerTest
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Ralf Eggert <r.eggert@travello.de>
+ * @link       http://www.ralfeggert.de/
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace CustomerTest\Hydrator;
+
+use PHPUnit_Framework_TestCase;
+use Zend\Stdlib\Hydrator\HydratorInterface;
+
+/**
+ * ModuleTest
+ *
+ * Tests the Module class of the Customer Module
+ *
+ * @package    CustomerTest
+ */
+class CustomerHydratorTest extends PHPUnit_Framework_TestCase
+{
+    public function testHydratorFileExistsAndIsInstantiable()
+    {
+        $className = 'Customer\Hydrator\CustomerHydrator';
+
+        $this->assertTrue(class_exists($className));
+
+        $customerHydrator = new $className();
+
+        $this->assertInstanceOf($className, $customerHydrator);
+        $this->assertTrue($$customerHydrator instanceof HydratorInterface);
+    }
+}
