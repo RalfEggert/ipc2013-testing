7b561158af2e27a706e29584d42a4ae51f165593
Ralf Eggert <ralf@travello.com>
2013-10-23 19:25:54 +0200

    CustomerHydrator erstellt

---
+++ module/Customer/src/Customer/Hydrator/CustomerHydrator.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Unit-Testing einer Zend Framework 2 Anwendung
+ *
+ * Zend Framework Session auf der International PHP Conference 2013 in München
+ *
+ * @package    Customer
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Ralf Eggert <r.eggert@travello.de>
+ * @link       http://www.ralfeggert.de/
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace Customer\Hydrator;
+use Zend\Stdlib\Hydrator\ClassMethods;
+
+/**
+ * Customer entity
+ *
+ * Hydrator class to represent customers
+ *
+ * @package    Customer
+ */
+class CustomerHydrator extends ClassMethods
+{
+}
