fac768bfb49a8374317955598ec27bce89f74dbd
Ralf Eggert <ralf@travello.com>
2013-10-23 18:23:01 +0200

    CustomerEntityTest erstellt

---
+++ module/Customer/tests/CustomerTest/Entity/CustomerEntityTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Unit-Testing einer Zend Framework 2 Anwendung
+ *
+ * Zend Framework Session auf der International PHP Conference 2013 in München
+ *
+ * @package    CustomerTest
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Ralf Eggert <r.eggert@travello.de>
+ * @link       http://www.ralfeggert.de/
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace CustomerTest\Entity;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * ModuleTest
+ *
+ * Tests the Module class of the Customer Module
+ *
+ * @package    CustomerTest
+ */
+class CustomerEntityTest extends PHPUnit_Framework_TestCase
+{
+    public function testEntityFileExists()
+    {
+        $this->assertTrue(class_exists('Customer\Entity\CustomerEntity'));
+    }
+}
