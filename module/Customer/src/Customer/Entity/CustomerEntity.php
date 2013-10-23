14a9d58edb7aba9845b2151d5241a07add0b6491
Ralf Eggert <ralf@travello.com>
2013-10-23 18:25:51 +0200

    CustomerEntity Klasse erstellt

---
+++ module/Customer/src/Customer/Entity/CustomerEntity.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Unit-Testing einer Zend Framework 2 Anwendung
+ *
+ * Zend Framework Session auf der International PHP Conference 2013 in München
+ *
+ * @package    Customer
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Ralf Eggert <r.eggert@travello.de>
+ * @link       http://www.ralfeggert.de/
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace Customer\Entity;
+
+/**
+ * Customer entity
+ *
+ * Entity class to represent customers
+ *
+ * @package    Customer
+ */
+class CustomerEntity
+{
+}
