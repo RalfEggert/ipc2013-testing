9b9bf48b213ca1f5ca6566d60367739e0c007186
Ralf Eggert <ralf@travello.com>
2013-10-24 23:04:11 +0200

    CustomerInputFilter erstellt

---
+++ module/Customer/src/Customer/InputFilter/CustomerInputFilter.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Unit-Testing einer Zend Framework 2 Anwendung
+ *
+ * Zend Framework Session auf der International PHP Conference 2013 in München
+ *
+ * @package    Customer
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Ralf Eggert <r.eggert@travello.de>
+ * @link       http://www.ralfeggert.de/
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace Customer\InputFilter;
+
+use Zend\InputFilter\InputFilter;
+
+/**
+ * Customer input filter
+ *
+ * InputFilter class to filter and validate customer data
+ *
+ * @package    Customer
+ */
+class CustomerInputFilter extends InputFilter
+{
+}
