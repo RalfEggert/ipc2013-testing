d7afb3e7e9117a913d4c46dfb16b8262feadf674
Ralf Eggert <ralf@travello.com>
2013-10-24 23:33:53 +0200

    CustomerInputFilter erweitert

--- module/Customer/src/Customer/InputFilter/CustomerInputFilter.php
+++ module/Customer/src/Customer/InputFilter/CustomerInputFilter.php
@@ -26,4 +26,141 @@ use Zend\InputFilter\InputFilter;
  */
 class CustomerInputFilter extends InputFilter
 {
+    /**
+     * Init
+     */
+    public function init()
+    {
+        $this->add(
+            array(
+                'name'       => 'id',
+                'required'   => true,
+                'validators' => array(
+                    array(
+                        'name' => 'Int',
+                    ),
+                ),
+            )
+        );
+
+        $this->add(
+            array(
+                'name'       => 'firstname',
+                'required'   => true,
+                'filters'    => array(
+                    array('name' => 'StringTrim'),
+                    array('name' => 'StringToLower'),
+                    array('name' => 'callback', 'options' => array('callback' => 'ucwords')),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'Alpha',
+                        'options' => array('allowWhiteSpace' => true)
+                    ),
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array('min' => '3', 'max' => '64')
+                    ),
+                ),
+            )
+        );
+
+        $this->add(
+            array(
+                'name'       => 'lastname',
+                'required'   => true,
+                'filters'    => array(
+                    array('name' => 'StringTrim'),
+                    array('name' => 'StringToLower'),
+                    array('name' => 'callback', 'options' => array('callback' => 'ucwords')),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'Alpha',
+                        'options' => array('allowWhiteSpace' => true)
+                    ),
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array('min' => '3', 'max' => '64')
+                    ),
+                ),
+            )
+        );
+
+        $this->add(
+            array(
+                'name'       => 'street',
+                'required'   => true,
+                'filters'    => array(
+                    array('name' => 'StringTrim'),
+                    array('name' => 'StringToLower'),
+                    array('name' => 'callback', 'options' => array('callback' => 'ucwords')),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'Alnum',
+                        'options' => array('allowWhiteSpace' => true)
+                    ),
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array('min' => '3', 'max' => '64')
+                    ),
+                ),
+            )
+        );
+
+        $this->add(
+            array(
+                'name'       => 'postcode',
+                'required'   => true,
+                'filters'    => array(
+                    array('name' => 'StringTrim'),
+                ),
+                'validators' => array(
+                    array(
+                        'name'   => 'Postcode',
+                        'locale' => 'de_DE',
+                    ),
+                ),
+            )
+        );
+
+        $this->add(
+            array(
+                'name'       => 'city',
+                'required'   => true,
+                'filters'    => array(
+                    array('name' => 'StringTrim'),
+                    array('name' => 'StringToLower'),
+                    array('name' => 'callback', 'options' => array('callback' => 'ucwords')),
+                ),
+                'validators' => array(
+                    array(
+                        'name'    => 'Alpha',
+                        'options' => array('allowWhiteSpace' => true)
+                    ),
+                    array(
+                        'name'    => 'StringLength',
+                        'options' => array('min' => '3', 'max' => '64')
+                    ),
+                ),
+            )
+        );
+
+        $this->add(
+            array(
+                'name'       => 'country',
+                'required'   => true,
+                'validators' => array(
+                    array(
+                        'name'    => 'InArray',
+                        'options' => array(
+                            'haystack' => array('de', 'at', 'ch'),
+                        ),
+                    ),
+                ),
+            )
+        );
+    }
+
 }
