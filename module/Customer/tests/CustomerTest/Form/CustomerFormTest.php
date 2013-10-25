9226fc5654f4175f6e5f53d48716fe8b25886e11
Ralf Eggert <ralf@travello.com>
2013-10-25 22:52:47 +0200

    CustomerFormTest erweitert

--- module/Customer/tests/CustomerTest/Form/CustomerFormTest.php
+++ module/Customer/tests/CustomerTest/Form/CustomerFormTest.php
@@ -61,4 +61,71 @@ class CustomerFormTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('span5', $firstnameElement->getAttribute('class'));
         $this->assertEquals('Vorname', $firstnameElement->getLabel());
     }
+
+    public function testLastnameElementExists()
+    {
+        $customerForm = new CustomerForm();
+        $customerForm->init();
+
+        $lastnameElement = $customerForm->get('lastname');
+
+        $this->assertEquals('text', $lastnameElement->getAttribute('type'));
+        $this->assertEquals('span5', $lastnameElement->getAttribute('class'));
+        $this->assertEquals('Nachname', $lastnameElement->getLabel());
+    }
+
+    public function testStreetElementExists()
+    {
+        $customerForm = new CustomerForm();
+        $customerForm->init();
+
+        $streetElement = $customerForm->get('street');
+
+        $this->assertEquals('text', $streetElement->getAttribute('type'));
+        $this->assertEquals('span5', $streetElement->getAttribute('class'));
+        $this->assertEquals('Straße', $streetElement->getLabel());
+    }
+
+    public function testPostcodeElementExists()
+    {
+        $customerForm = new CustomerForm();
+        $customerForm->init();
+
+        $postcodeElement = $customerForm->get('postcode');
+
+        $this->assertEquals('text', $postcodeElement->getAttribute('type'));
+        $this->assertEquals('span5', $postcodeElement->getAttribute('class'));
+        $this->assertEquals('PLZ', $postcodeElement->getLabel());
+    }
+
+    public function testCityElementExists()
+    {
+        $customerForm = new CustomerForm();
+        $customerForm->init();
+
+        $cityElement = $customerForm->get('city');
+
+        $this->assertEquals('text', $cityElement->getAttribute('type'));
+        $this->assertEquals('span5', $cityElement->getAttribute('class'));
+        $this->assertEquals('Stadt', $cityElement->getLabel());
+    }
+
+    public function testCountryElementExists()
+    {
+        $valueOptions = array(
+            'de' => 'Deutschland',
+            'at' => 'Österreich',
+            'ch' => 'Schweiz',
+        );
+
+        $customerForm = new CustomerForm();
+        $customerForm->init();
+
+        $countryElement = $customerForm->get('country');
+
+        $this->assertEquals('select', $countryElement->getAttribute('type'));
+        $this->assertEquals('span5', $countryElement->getAttribute('class'));
+        $this->assertEquals('Land', $countryElement->getLabel());
+        $this->assertEquals($valueOptions, $countryElement->getValueOptions);
+    }
 }
