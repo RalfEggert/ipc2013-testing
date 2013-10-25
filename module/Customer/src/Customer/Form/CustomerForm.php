df9af10a4c2444b57925a644587b4c990e8cfe72
Ralf Eggert <ralf@travello.com>
2013-10-25 22:36:38 +0200

    CustomerForm erstellt

---
+++ module/Customer/src/Customer/Form/CustomerForm.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Unit-Testing einer Zend Framework 2 Anwendung
+ *
+ * Zend Framework Session auf der International PHP Conference 2013 in München
+ *
+ * @package    Customer
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Ralf Eggert <r.eggert@travello.de>
+ * @link       http://www.ralfeggert.de/
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace Customer\Form;
+
+use Zend\Form\Form;
+
+/**
+ * Customer form
+ *
+ * Form class to create and update customer data
+ *
+ * @package    Customer
+ */
+class CustomerForm extends Form
+{
+    /**
+     * Init
+     */
+    public function init()
+    {
+
+    }
+}
