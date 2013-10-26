aad3a94f6fa6ec7cc944fb425a3c9f2068d0f38e
Ralf Eggert <ralf@travello.com>
2013-10-26 07:36:51 +0200

    CustomerServiceTest erstellt

---
+++ module/Customer/tests/CustomerTest/Service/CustomerServiceTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Unit-Testing einer Zend Framework 2 Anwendung
+ *
+ * Zend Framework Session auf der International PHP Conference 2013 in München
+ *
+ * @package    CustomerTest
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Ralf Eggert <r.eggert@travello.de>
+ * @link       http://www.ralfeggert.de/
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace CustomerTest\Service;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * CustomerServiceTest
+ *
+ * Tests the customer service class of the Customer Module
+ *
+ * @package    CustomerTest
+ */
+class CustomerServiceTest extends PHPUnit_Framework_TestCase
+{
+    public function testServiceFileExistsAndIsInstantiable()
+    {
+        $className = 'Customer\Service\CustomerService';
+
+        $this->assertTrue(class_exists($className));
+
+        $customerService = new $className();
+
+        $this->assertInstanceOf($className, $customerService);
+    }
+}
