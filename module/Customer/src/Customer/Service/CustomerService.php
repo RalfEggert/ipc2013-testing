3223175243e23627ae6c248383963320123dfdcf
Ralf Eggert <ralf@travello.com>
2013-10-26 07:38:07 +0200

    CustomerService erstellt

---
+++ module/Customer/src/Customer/Service/CustomerService.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Unit-Testing einer Zend Framework 2 Anwendung
+ *
+ * Zend Framework Session auf der International PHP Conference 2013 in München
+ *
+ * @package    Customer
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Ralf Eggert <r.eggert@travello.de>
+ * @link       http://www.ralfeggert.de/
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace Customer\Service;
+
+/**
+ * Customer sercice
+ *
+ * Service class to handle the customer
+ *
+ * @package    Customer
+ */
+class CustomerService
+{
+}
